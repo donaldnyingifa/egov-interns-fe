6b99e2eacfe8718126e9364d93c0cb91a1bbe17a
Samuel Adekoya <samueladexx@gmail.com>
2024-06-13 11:41:52 +0100

    fixed linting issues

--- src/components/EditProfile.tsx
+++ src/components/EditProfile.tsx
@@ -89,7 +89,7 @@ export const EditProfile = () => {
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
 
