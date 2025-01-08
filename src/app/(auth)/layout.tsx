ab726afc49e730b45639aa5583ef2f93d538d37e
Samuel Adekoya <samueladexx@gmail.com>
2025-01-08 20:56:56 +0100

    redesign dashboard navigation

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { useAuth } from "@/hooks/useAuth";
 import { usePathname, useRouter } from "next/navigation";
 import { Loader } from "@/components/Loader";
-import { toast } from "@/components/ui/use-toast";
-import { Button } from "@/components/ui/button";
 import { HomeIcon, User } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const { user, loading } = useAuth();
   const router = useRouter();
   const pathname = usePathname();
+  const [showFallbackImage, setShowFallbackImage] = useState(false);
 
   useEffect(() => {
     if (!loading && !user) {
@@ -25,34 +25,58 @@ const Layout = ({ children }: { children: ReactNode }) => {
   }
 
   return (
-    <div className="flex max-w-lg mx-auto border min-h-screen relative">
-      <div className="flex-grow"> {children}</div>
-
-      <div className="w-full absolute bottom-4">
-        <div className="rounded-lg p-1 border-2 border-slate-700 w-[80%] md:w-[60%] mx-auto flex gap-2">
-          <Button
-            className="flex items-center flex-grow gap-2"
-            asChild
-            size="sm"
-            variant={pathname === "/home" ? "default" : "outline"}
+    <div className="flex max-w-2xl mx-auto border min-h-screen relative">
+      <div className="w-15 md:w-36 p-1 border">
+        <div className="rounded-lg p-1 w-full mx-auto flex flex-col gap-2">
+          <Link
+            href="/home"
+            className={`flex items-center md:gap-2 p-2 rounded-md text-white ${
+              pathname === "/home"
+                ? "bg-slate-900 text-white"
+                : "bg-white text-slate-900"
+            }`}
           >
-            <Link href="/home">
-              <span>Home</span> <HomeIcon className="w-4 h-4" />
-            </Link>
-          </Button>
+            <HomeIcon className="w-6 h-6" />
+            <p className="hidden md:block text-sm">Home</p>{" "}
+          </Link>
 
-          <Button
-            className="flex items-center flex-grow gap-2 border-[1.5px]"
-            asChild
-            size="sm"
-            variant={pathname === `/${user.username}` ? "default" : "outline"}
+          <Link
+            href={`/${user.username}`}
+            className={`flex items-center md:gap-2 p-2 rounded-md  ${
+              pathname === `/${user.username}`
+                ? "bg-slate-900 text-white"
+                : "bg-white text-slate-900"
+            }`}
           >
-            <Link href={`/${user.username}`}>
-              <span>Profile</span> <User className="w-4 h-4" />{" "}
+            {/* <User className="w-5 h-5" /> */}
+            <Link
+              href={`/${user.username}`}
+              className="w-6 h-6 block rounded-full overflow-hidden"
+            >
+              {showFallbackImage ? (
+                <div className="grid place-items-center w-full h-full">
+                  <span>
+                    {`${user.firstName[0].toUpperCase()}${user.lastName[0].toUpperCase()}`}
+                  </span>
+                  <span className="sr-only">Profile Image</span>
+                </div>
+              ) : (
+                <Image
+                  src={user.profileImage ?? "/"}
+                  alt="Profile Image"
+                  width={300}
+                  height={300}
+                  className="object-cover object-center w-full h-full"
+                  onError={() => setShowFallbackImage(true)}
+                  onLoad={() => setShowFallbackImage(false)}
+                />
+              )}
             </Link>
-          </Button>
+            <p className="hidden md:block text-sm">Profile</p>
+          </Link>
         </div>
       </div>
+      <div className="flex-grow"> {children}</div>
     </div>
   );
 };
